58ab09d2008caab70ec84c5f35ad5127509a08bd
PsypherX-Dev <142802486+PsypherX-Dev@users.noreply.github.com>
2023-08-28 11:13:48 +0200

    Update main.js - Removed unused js code

--- main.js
+++ main.js
@@ -1,22 +1,8 @@
-window.onload = function() {
-    
-}
-
 window.onscroll = function () {
     scrollFunction();
 }
 
 function scrollFunction () {
-    // NavBar
-    // if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-    //     document.getElementById("navbar").style.opacity = 0.5;
-    //     document.getElementById("navbar").style.transition = "opacity 0.3s";
-    // }
-    // else {
-    //     document.getElementById("navbar").style.opacity = 1;
-    //     document.getElementById("navbar").style.transition = "opacity 0.3s";
-    // }
-
     // Scroll To Top Button
     if (document.documentElement.scrollTop > 700) {
         document.getElementById("scrolltotop_parent").style.opacity = 1;
